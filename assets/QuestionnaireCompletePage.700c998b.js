ce53e3ae5ac0452cceac7c8e17a7bc70c74b8f59
Dominic Willi <dominic.willi@students.bfh.ch>
2024-04-26 18:18:06 +0200

    update

---
+++ assets/QuestionnaireCompletePage.700c998b.js
@@ -0,0 +1,55 @@
+import { ak as openBlock, al as createBlock, am as withCtx, k as createVNode, az as QCardSection, aq as QIcon, aA as createBaseVNode, ap as toDisplayString, aB as QCardActions, an as QBtn, as as unref, ao as createTextVNode, aC as QCard } from "./index.38afc488.js";
+import { Q as QPage } from "./QPage.1212f342.js";
+import { f as fhir } from "./midataService.666dd6d8.js";
+const _hoisted_1 = { class: "text-h6" };
+const _hoisted_2 = { class: "text-center" };
+const _sfc_main = {
+  __name: "QuestionnaireCompletePage",
+  setup(__props) {
+    return (_ctx, _cache) => {
+      return openBlock(), createBlock(QPage, { padding: "" }, {
+        default: withCtx(() => [
+          createVNode(QCard, { class: "q-mb-sm" }, {
+            default: withCtx(() => [
+              createVNode(QCardSection, { class: "text-center" }, {
+                default: withCtx(() => [
+                  createVNode(QIcon, {
+                    name: "task",
+                    size: "xl",
+                    class: "text-green"
+                  }),
+                  createBaseVNode("div", _hoisted_1, toDisplayString(_ctx.$t("questionnaireDonePageTitle")), 1)
+                ]),
+                _: 1
+              }),
+              createVNode(QCardSection, null, {
+                default: withCtx(() => [
+                  createBaseVNode("div", _hoisted_2, toDisplayString(_ctx.$t("questionnaireDonePageText")), 1)
+                ]),
+                _: 1
+              }),
+              createVNode(QCardActions, { align: "center" }, {
+                default: withCtx(() => [
+                  createVNode(QBtn, {
+                    color: "primary",
+                    to: "/infoPage",
+                    onClick: _cache[0] || (_cache[0] = ($event) => unref(fhir).logout())
+                  }, {
+                    default: withCtx(() => [
+                      createTextVNode("Logout")
+                    ]),
+                    _: 1
+                  })
+                ]),
+                _: 1
+              })
+            ]),
+            _: 1
+          })
+        ]),
+        _: 1
+      });
+    };
+  }
+};
+export { _sfc_main as default };
---
+++ assets/WelcomePage.726c7939.js
@@ -0,0 +1,63 @@
+import { ak as openBlock, al as createBlock, am as withCtx, as as unref, at as createCommentVNode, aA as createBaseVNode, aC as QCard, k as createVNode, aq as QIcon, ap as toDisplayString, az as QCardSection, ao as createTextVNode, an as QBtn, aB as QCardActions } from "./index.38afc488.js";
+import { Q as QPage } from "./QPage.1212f342.js";
+import { L as LoginCard } from "./LoginCard.baa44388.js";
+import { u as userStore } from "./store.66b8a5fd.js";
+import "./midataService.666dd6d8.js";
+const _hoisted_1 = { class: "q-py-md row justify-center" };
+const _hoisted_2 = { class: "text-h6" };
+const _sfc_main = {
+  __name: "WelcomePage",
+  setup(__props) {
+    const store = userStore();
+    return (_ctx, _cache) => {
+      return openBlock(), createBlock(QPage, null, {
+        default: withCtx(() => [
+          !unref(store).isLoggedIn ? (openBlock(), createBlock(LoginCard, { key: 0 })) : createCommentVNode("", true),
+          createBaseVNode("div", _hoisted_1, [
+            unref(store).isLoggedIn ? (openBlock(), createBlock(QCard, {
+              key: 0,
+              class: "col-sm-8"
+            }, {
+              default: withCtx(() => [
+                createVNode(QCardSection, { class: "text-center" }, {
+                  default: withCtx(() => [
+                    createVNode(QIcon, {
+                      name: "check_circle_outline",
+                      size: "xl",
+                      class: "text-green"
+                    }),
+                    createBaseVNode("div", _hoisted_2, toDisplayString(_ctx.$t("registerSuccess")), 1)
+                  ]),
+                  _: 1
+                }),
+                createVNode(QCardSection, { class: "text-center" }, {
+                  default: withCtx(() => [
+                    createTextVNode(toDisplayString(_ctx.$t("loginSuccess")), 1)
+                  ]),
+                  _: 1
+                }),
+                createVNode(QCardActions, { align: "center" }, {
+                  default: withCtx(() => [
+                    createVNode(QBtn, {
+                      color: "primary",
+                      to: "/questionnaire"
+                    }, {
+                      default: withCtx(() => [
+                        createTextVNode(toDisplayString(_ctx.$t("startButton")), 1)
+                      ]),
+                      _: 1
+                    })
+                  ]),
+                  _: 1
+                })
+              ]),
+              _: 1
+            })) : createCommentVNode("", true)
+          ])
+        ]),
+        _: 1
+      });
+    };
+  }
+};
+export { _sfc_main as default };
