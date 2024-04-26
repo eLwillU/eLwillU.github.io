9007b051ee7ca9399c7d6310a0c5e77e0a385d7b
Dominic Willi <dominic.willi@students.bfh.ch>
2024-04-26 15:58:46 +0200

    added new project build with midata-prod connection

---
+++ assets/QCardActions.a562861a.js
@@ -0,0 +1,92 @@
+import { c as createComponent, h as hSlot, o as useAlignProps, p as useAlign } from "./QBtn.7eb97a4e.js";
+import { c as computed, h, g as getCurrentInstance, i as inject, I as emptyRenderFn, J as layoutKey, S as pageContainerKey } from "./index.74229698.js";
+import { u as useDarkProps, a as useDark } from "./use-dark.2df1d8b3.js";
+var QCardSection = createComponent({
+  name: "QCardSection",
+  props: {
+    tag: {
+      type: String,
+      default: "div"
+    },
+    horizontal: Boolean
+  },
+  setup(props, { slots }) {
+    const classes = computed(
+      () => `q-card__section q-card__section--${props.horizontal === true ? "horiz row no-wrap" : "vert"}`
+    );
+    return () => h(props.tag, { class: classes.value }, hSlot(slots.default));
+  }
+});
+var QCard = createComponent({
+  name: "QCard",
+  props: {
+    ...useDarkProps,
+    tag: {
+      type: String,
+      default: "div"
+    },
+    square: Boolean,
+    flat: Boolean,
+    bordered: Boolean
+  },
+  setup(props, { slots }) {
+    const { proxy: { $q } } = getCurrentInstance();
+    const isDark = useDark(props, $q);
+    const classes = computed(
+      () => "q-card" + (isDark.value === true ? " q-card--dark q-dark" : "") + (props.bordered === true ? " q-card--bordered" : "") + (props.square === true ? " q-card--square no-border-radius" : "") + (props.flat === true ? " q-card--flat no-shadow" : "")
+    );
+    return () => h(props.tag, { class: classes.value }, hSlot(slots.default));
+  }
+});
+var QPage = createComponent({
+  name: "QPage",
+  props: {
+    padding: Boolean,
+    styleFn: Function
+  },
+  setup(props, { slots }) {
+    const { proxy: { $q } } = getCurrentInstance();
+    const $layout = inject(layoutKey, emptyRenderFn);
+    if ($layout === emptyRenderFn) {
+      console.error("QPage needs to be a deep child of QLayout");
+      return emptyRenderFn;
+    }
+    const $pageContainer = inject(pageContainerKey, emptyRenderFn);
+    if ($pageContainer === emptyRenderFn) {
+      console.error("QPage needs to be child of QPageContainer");
+      return emptyRenderFn;
+    }
+    const style = computed(() => {
+      const offset = ($layout.header.space === true ? $layout.header.size : 0) + ($layout.footer.space === true ? $layout.footer.size : 0);
+      if (typeof props.styleFn === "function") {
+        const height = $layout.isContainer.value === true ? $layout.containerHeight.value : $q.screen.height;
+        return props.styleFn(offset, height);
+      }
+      return {
+        minHeight: $layout.isContainer.value === true ? $layout.containerHeight.value - offset + "px" : $q.screen.height === 0 ? offset !== 0 ? `calc(100vh - ${offset}px)` : "100vh" : $q.screen.height - offset + "px"
+      };
+    });
+    const classes = computed(
+      () => `q-page${props.padding === true ? " q-layout-padding" : ""}`
+    );
+    return () => h("main", {
+      class: classes.value,
+      style: style.value
+    }, hSlot(slots.default));
+  }
+});
+var QCardActions = createComponent({
+  name: "QCardActions",
+  props: {
+    ...useAlignProps,
+    vertical: Boolean
+  },
+  setup(props, { slots }) {
+    const alignClass = useAlign(props);
+    const classes = computed(
+      () => `q-card__actions ${alignClass.value} q-card__actions--${props.vertical === true ? "vert column" : "horiz row"}`
+    );
+    return () => h("div", { class: classes.value }, hSlot(slots.default));
+  }
+});
+export { QCardSection as Q, QCardActions as a, QCard as b, QPage as c };
