5d4839f73a40877e84f5d3213bc11405bd03f342
Dominic Willi <dominic.willi@students.bfh.ch>
2024-05-02 09:10:22 +0200

    fixed language bug

---
+++ assets/i18n.8c3dbd16.js
@@ -0,0 +1,216 @@
+import { b as boot } from "./index.61612f03.js";
+import { c as createI18n } from "./vue-i18n.09c58da5.js";
+import { u as userStore } from "./store.0e34a26b.js";
+const welcome$1 = "Willkommen";
+const infoWelcome$1 = "Willkommen bei REVIVAL";
+const infoCaption$1 = " Lasst uns zusammen die Krebsbehandlung besser machen \u2013 Ihre Stimme z\xE4hlt!";
+const infoText$1 = "Nutzen Sie jetzt die Gelegenheit, die Zukunft der Krebsbehandlung aktiv mitzugestalten, indem Sie Ihre pers\xF6nlichen Erfahrungen durch den digitalen SCAPE-Fragebogen teilen. Ihr wertvolles Feedback ist ein Schl\xFCssel zu einer besseren Patientenversorgung und hilft uns, das Gesundheitssystem effektiver zu gestalten. REVIVAL ist mehr als nur ein Studentenprojekt \u2013 es ist eine Bewegung, die durch Ihre Teilnahme und Ihr Engagement w\xE4chst. Lassen Sie uns gemeinsam handeln, um den Weg f\xFCr innovative Ver\xE4nderungen zu ebnen und das Wohl von Krebspatienten zu f\xF6rdern. F\xFCr einen tieferen Einblick und weitere Informationen schauen Sie sich bitte das Video unten an und entdecken Sie, wie auch Sie einen Unterschied machen k\xF6nnen. Seien Sie der Wandel, den Sie in der Welt sehen m\xF6chten \u2013 beginnen wir heute!";
+const yourAnswerOption$1 = "Ihre Antwort:";
+const back$1 = "Zur\xFCck";
+const next$1 = "Weiter";
+const question$1 = "Frage";
+const from$1 = "von";
+const loginSuccess$1 = "Sie haben sich erfolgreich eingeloggt!";
+const clickToStart$1 = "Klicken Sie hier um den Fragebogen auszuf\xFCllen:";
+const startButton$1 = "Fragebogen ausf\xFCllen";
+const loginCardText$1 = "Damit sie REVIVAL verwenden k\xF6nnen, m\xFCssen Sie sich zuerst einloggen/registrieren.";
+const connectToMidata$1 = "Mit MIDATA verbinden";
+const completeQuestionnaire$1 = "Beenden";
+const infoVideo$1 = "Informationsvideo";
+const moreInfo$1 = "Weitere Informationen";
+const interestedPersonM$1 = "lieber Interessent";
+const interestedPersonF$1 = "Liebe Interessentin";
+const informationPage$1 = "Informationsseite";
+const startingPage$1 = "Startseite";
+const questionnaire$1 = "Fragebogen";
+const infoPageIntroText$1 = "Wir, Dominic und Karin, sind fast fertig mit unserem Studium in Medizininformatik. In unserer Bachelorarbeit erarbeiten wir ein Projekt, das uns sehr am Herzen liegt: Wir m\xF6chten Ihre Stimme und Erfahrungen in den Mittelpunkt stellen.";
+const infoPageAimTitle$1 = "Unser Ziel?";
+const infoPageAimText$1 = "Wir m\xF6chten mit Ihrer Unterst\xFCtzung und Ihren direkten Erfahrungen das Cancer Survivors Support Programm (CSSP) mitgestalten. Das Cancer Survivors Support Programm ist ein Projekt der Krebsliga.";
+const csspInfoButton$1 = "Mehr zum CSSP";
+const infoPageThesisTitle$1 = "Unsere Bachelorarbeit?";
+const infoPageThesisText$1 = "Der SCAPE-CH-Fragebogen ist ein Fragebogen zur Erfassung von Erfahrungen von Krebspatienten in der Versorgung. Dieser wird bereits erfolgreich in der Schweiz eingesetzt. Wir m\xF6chten diesen Fragebogen digitalisieren und f\xFCr Sie leicht zug\xE4nglich machen.";
+const infoPageRoleTitle$1 = "Ihre Rolle?";
+const infoPageRoleText$1 = "Sie sind der Schl\xFCssel zur Verbesserung der Behandlungsprozesse. Denn nur durch Ihre Er-fahrungen kann sichergestellt werden, dass die Betreuung genau auf Ihre Bed\xFCrfnisse zuge-schnitten ist.";
+const infoPageToDoTitle$1 = "Was ist zu tun?";
+const infoPageToDoText$1 = "F\xFCllen Sie unseren digitalen Fragebogen aus \u2013 es dauert ca. 10-15 Minuten, abh\xE4ngig von Ihrer Vertrautheit mit Ihrem Mobilger\xE4t. Daf\xFCr m\xFCssen Sie ein Konto auf der MIDATA-Plattform er\xF6ffnen.";
+const infoPageMoreTitle$1 = "Mehr erfahren?";
+const infoPageMoreText$1 = "Schauen Sie unser Video an! Bei Fragen stehen wir per Email zur Verf\xFCgung. Auf Anfrage k\xF6nnen wir Sie gerne telefonisch Beraten.";
+const infoPageTogether$1 = "Wir w\xFCrden uns auf eine Zusammenarbeit freuen.";
+const infoPageGreeting$1 = "Herzliche Gr\xFCsse Dominic Willi und Karin Kennel";
+const registerNowButton$1 = "Registrieren Sie sich jetzt!";
+const scapeQuestionnaireInfo$1 = "Was ist der SCAPE-Fragebogen?";
+const SCAPEReadMore$1 = "Lesen Sie hier weiter";
+const language$1 = "Sprache";
+const settings$1 = "Einstellungen";
+const registerSuccess$1 = "Login/Registrierung erfolgreich";
+const questionnaireDonePageTitle$1 = "Fragebogen ausgef\xFCllt!";
+const questionnaireDonePageText$1 = "Sie haben den Fragebogen erfolgreich ausgef\xFCllt, vielen Dank! Wir werden die Daten nun im Rahmen unserer Bachelorarbeit auswerten.";
+const errorPage$1 = "Ups. Nichts da...";
+const confirmEnd$1 = "M\xF6chten Sie den Fragebogen abschliessen und abschicken?";
+const cancel$1 = "Abbrechen";
+const send$1 = "Senden";
+const termsofuse$1 = "Nutzungsbedingungen und Datenschutzerkl\xE4rung";
+var DE = {
+  welcome: welcome$1,
+  infoWelcome: infoWelcome$1,
+  infoCaption: infoCaption$1,
+  infoText: infoText$1,
+  yourAnswerOption: yourAnswerOption$1,
+  back: back$1,
+  next: next$1,
+  question: question$1,
+  from: from$1,
+  loginSuccess: loginSuccess$1,
+  clickToStart: clickToStart$1,
+  startButton: startButton$1,
+  loginCardText: loginCardText$1,
+  connectToMidata: connectToMidata$1,
+  completeQuestionnaire: completeQuestionnaire$1,
+  infoVideo: infoVideo$1,
+  moreInfo: moreInfo$1,
+  interestedPersonM: interestedPersonM$1,
+  interestedPersonF: interestedPersonF$1,
+  informationPage: informationPage$1,
+  startingPage: startingPage$1,
+  questionnaire: questionnaire$1,
+  infoPageIntroText: infoPageIntroText$1,
+  infoPageAimTitle: infoPageAimTitle$1,
+  infoPageAimText: infoPageAimText$1,
+  csspInfoButton: csspInfoButton$1,
+  infoPageThesisTitle: infoPageThesisTitle$1,
+  infoPageThesisText: infoPageThesisText$1,
+  infoPageRoleTitle: infoPageRoleTitle$1,
+  infoPageRoleText: infoPageRoleText$1,
+  infoPageToDoTitle: infoPageToDoTitle$1,
+  infoPageToDoText: infoPageToDoText$1,
+  infoPageMoreTitle: infoPageMoreTitle$1,
+  infoPageMoreText: infoPageMoreText$1,
+  infoPageTogether: infoPageTogether$1,
+  infoPageGreeting: infoPageGreeting$1,
+  registerNowButton: registerNowButton$1,
+  scapeQuestionnaireInfo: scapeQuestionnaireInfo$1,
+  SCAPEReadMore: SCAPEReadMore$1,
+  language: language$1,
+  settings: settings$1,
+  registerSuccess: registerSuccess$1,
+  questionnaireDonePageTitle: questionnaireDonePageTitle$1,
+  questionnaireDonePageText: questionnaireDonePageText$1,
+  errorPage: errorPage$1,
+  confirmEnd: confirmEnd$1,
+  cancel: cancel$1,
+  send: send$1,
+  termsofuse: termsofuse$1
+};
+const welcome = "Bonjour";
+const infoWelcome = "Bienvenue chez REVIVAL";
+const infoCaption = "Ensemble, am\xE9liorons le traitement du cancer - votre voix compte !";
+const infoText = "Saisissez d\xE8s maintenant l'occasion de participer activement \xE0 la construction de l'avenir du traitement du cancer en partageant votre exp\xE9rience personnelle par le biais du questionnaire num\xE9rique SCAPE. Votre pr\xE9cieux feedback est la cl\xE9 d'une meilleure prise en charge des patients et nous aide \xE0 rendre le syst\xE8me de sant\xE9 plus efficace. REVIVAL est plus qu'un simple projet \xE9tudiant - c'est un mouvement qui grandit gr\xE2ce \xE0 votre participation et \xE0 votre engagement. Agissons ensemble afin d'ouvrir la voie \xE0 des changements innovants et de promouvoir le bien-\xEAtre des patients atteints de cancer. Pour un aper\xE7u plus approfondi et de plus amples informations, veuillez regarder la vid\xE9o ci-dessous et d\xE9couvrir comment vous pouvez, vous aussi, faire la diff\xE9rence. Soyez le changement que vous voulez voir dans le monde - commen\xE7ons d\xE8s aujourd'hui !";
+const yourAnswerOption = "Votre r\xE9ponse :";
+const back = "Pr\xE9c\xE9dent";
+const next = "Suivant";
+const question = "Question";
+const from = "de";
+const loginSuccess = "Vous vous \xEAtes connect\xE9 avec succ\xE8s !";
+const clickToStart = "Cliquez ici pour remplir le questionnaire :";
+const startButton = "Remplir le questionnaire";
+const loginCardText = "Pour pouvoir utiliser REVIVAL, vous devez d'abord vous connecter. vous connecter/enregistrer.";
+const connectToMidata = "Connecter \xE0 MIDATA";
+const completeQuestionnaire = "Terminer";
+const infoVideo = "Vid\xE9o d'information";
+const moreInfo = "Informations compl\xE9mentaires";
+const interestedPersonM = "cher int\xE9ress\xE9";
+const interestedPersonF = "Ch\xE8re int\xE9ress\xE9e";
+const informationPage = "Site d'information";
+const startingPage = "Page d'accueil";
+const questionnaire = "Questionnaire";
+const infoPageIntroText = "Nous, Dominic et Karin, avons presque termin\xE9 nos \xE9tudes en informatique m\xE9dicale. Dans notre travail de bachelor, nous \xE9laborons un projet qui nous tient \xE0 c\u0153ur : Nous voulons mettre votre voix et vos exp\xE9riences au centre de nos pr\xE9occupations.";
+const infoPageAimTitle = "Notre objectif ?";
+const infoPageAimText = "Nous souhaitons contribuer au programme de soutien aux survivants du cancer (CSSP) gr\xE2ce \xE0 votre soutien et \xE0 vos exp\xE9riences directes. Le Programme de soutien aux survivants du cancer est un projet de la Ligue contre le cancer.";
+const csspInfoButton = "En savoir plus sur la CSSP";
+const infoPageThesisTitle = "Notre travail de bachelor ?";
+const infoPageThesisText = "Le questionnaire SCAPE-CH est un questionnaire destin\xE9 \xE0 recueillir les exp\xE9riences des patients atteints de cancer en mati\xE8re de soins. Il est d\xE9j\xE0 utilis\xE9 avec succ\xE8s en Suisse. Nous souhaitons num\xE9riser ce questionnaire et le rendre facilement accessible pour vous.";
+const infoPageRoleTitle = "Votre r\xF4le ?";
+const infoPageRoleText = "Vous \xEAtes la cl\xE9 de l'am\xE9lioration des processus de traitement. En effet, seule votre exp\xE9rience permet de garantir que les soins sont adapt\xE9s \xE0 vos besoins. La prise en charge est adapt\xE9e \xE0 vos besoins.";
+const infoPageToDoTitle = "Que faire ?";
+const infoPageToDoText = "Remplissez notre questionnaire num\xE9rique - cela prend environ 10 \xE0 15 minutes, en fonction de votre familiarit\xE9 avec votre appareil mobile. Pour cela, vous devez ouvrir un compte sur la plateforme MIDATA.";
+const infoPageMoreTitle = "En savoir plus ?";
+const infoPageMoreText = "Regardez notre vid\xE9o ! Si vous avez des questions, nous sommes \xE0 votre disposition par e-mail. Sur demande, nous pouvons vous conseiller par t\xE9l\xE9phone.";
+const infoPageTogether = "Nous serions ravis de travailler ensemble.";
+const infoPageGreeting = "Cordiales salutations Dominic Willi et Karin Kennel";
+const registerNowButton = "Inscrivez-vous maintenant !";
+const scapeQuestionnaireInfo = "Qu'est-ce que le questionnaire SCAPE ?";
+const SCAPEReadMore = "Lisez la suite ici";
+const language = "Langue";
+const settings = "Param\xE8tres";
+const registerSuccess = "Connexion/enregistrement r\xE9ussi(e).";
+const questionnaireDonePageTitle = "Questionnaire rempli !";
+const questionnaireDonePageText = "Vous avez rempli le questionnaire avec succ\xE8s, merci beaucoup ! Nous allons maintenant \xE9valuer les donn\xE9es dans le cadre de notre travail de bachelor.";
+const errorPage = "Oups. Rien ici...";
+const confirmEnd = "Souhaitez-vous terminer et envoyer le questionnaire ?";
+const cancel = "Annuler";
+const send = "Envoyer";
+const termsofuse = "Conditions d'utilisation et d\xE9claration de protection des donn\xE9es";
+var FR = {
+  welcome,
+  infoWelcome,
+  infoCaption,
+  infoText,
+  yourAnswerOption,
+  back,
+  next,
+  question,
+  from,
+  loginSuccess,
+  clickToStart,
+  startButton,
+  loginCardText,
+  connectToMidata,
+  completeQuestionnaire,
+  infoVideo,
+  moreInfo,
+  interestedPersonM,
+  interestedPersonF,
+  informationPage,
+  startingPage,
+  questionnaire,
+  infoPageIntroText,
+  infoPageAimTitle,
+  infoPageAimText,
+  csspInfoButton,
+  infoPageThesisTitle,
+  infoPageThesisText,
+  infoPageRoleTitle,
+  infoPageRoleText,
+  infoPageToDoTitle,
+  infoPageToDoText,
+  infoPageMoreTitle,
+  infoPageMoreText,
+  infoPageTogether,
+  infoPageGreeting,
+  registerNowButton,
+  scapeQuestionnaireInfo,
+  SCAPEReadMore,
+  language,
+  settings,
+  registerSuccess,
+  questionnaireDonePageTitle,
+  questionnaireDonePageText,
+  errorPage,
+  confirmEnd,
+  cancel,
+  send,
+  termsofuse
+};
+const store = userStore();
+var i18n = boot(({ app }) => {
+  const i18n2 = createI18n({
+    locale: store.locale,
+    messages: {
+      fr: FR,
+      de: DE
+    },
+    legacy: false
+  });
+  app.use(i18n2);
+});
+export { i18n as default };
