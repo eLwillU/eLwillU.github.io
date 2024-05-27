31c34362f5d60ce1b473092749108a477e70e0a1
Dominic Willi <dominic.willi@students.bfh.ch>
2024-05-27 10:09:46 +0200

    added new page

---
+++ assets/FeedbackPage.51ca9e08.js
@@ -0,0 +1,51 @@
+import { Q as QImg } from "./QImg.df351708.js";
+import { aw as _export_sfc, a3 as openBlock, a4 as createBlock, a5 as withCtx, k as createVNode, ax as QCardSection, az as QCardActions, a6 as QBtn, a7 as createTextVNode, aA as QCard, ay as createBaseVNode } from "./index.73cef502.js";
+import { Q as QPage } from "./QPage.50568207.js";
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createBaseVNode("div", { class: "absolute-bottom" }, [
+  /* @__PURE__ */ createBaseVNode("div", { class: "text-h6" }, "Willkommen bei REVIVAL")
+], -1);
+const _hoisted_2 = /* @__PURE__ */ createBaseVNode("div", null, " Vielen Dank f\xFCr Ihr Interesse! Sie haben nun die M\xF6glichkeit entweder unsere komplette Web-App mit dem originalen Fragebogen zu testen. Oder Sie haben die M\xF6glichkeit eine kurze Version eines Fragebogens durchzuspielen, um ein Feedback zum Tech-Day zu geben. ", -1);
+function _sfc_render(_ctx, _cache) {
+  return openBlock(), createBlock(QPage, null, {
+    default: withCtx(() => [
+      createVNode(QCard, { class: "q-mt-sm" }, {
+        default: withCtx(() => [
+          createVNode(QImg, { src: "/images/1 nSqr-piDM2QmdAWzGI0DuA.png" }, {
+            default: withCtx(() => [
+              _hoisted_1
+            ]),
+            _: 1
+          }),
+          createVNode(QCardSection, null, {
+            default: withCtx(() => [
+              _hoisted_2
+            ]),
+            _: 1
+          }),
+          createVNode(QCardActions, null, {
+            default: withCtx(() => [
+              createVNode(QBtn, { color: "primary" }, {
+                default: withCtx(() => [
+                  createTextVNode("Ganze Web-App")
+                ]),
+                _: 1
+              }),
+              createVNode(QBtn, { color: "primary" }, {
+                default: withCtx(() => [
+                  createTextVNode("Verk\xFCrzter Fragebogen")
+                ]),
+                _: 1
+              })
+            ]),
+            _: 1
+          })
+        ]),
+        _: 1
+      })
+    ]),
+    _: 1
+  });
+}
+var FeedbackPage = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { FeedbackPage as default };
---
+++ assets/QuestionnaireCompletePage.efa15774.js
@@ -0,0 +1,56 @@
+import { a3 as openBlock, a4 as createBlock, a5 as withCtx, k as createVNode, ax as QCardSection, a9 as QIcon, ay as createBaseVNode, a8 as toDisplayString, az as QCardActions, a6 as QBtn, ab as unref, a7 as createTextVNode, aA as QCard } from "./index.73cef502.js";
+import { Q as QPage } from "./QPage.50568207.js";
+import { fhir } from "./midataService.acf51e2d.js";
+import "./index.33e8d543.js";
+const _hoisted_1 = { class: "text-h6" };
+const _hoisted_2 = { class: "text-center" };
+const _sfc_main = {
+  __name: "QuestionnaireCompletePage",
+  setup(__props) {
+    return (_ctx, _cache) => {
+      return openBlock(), createBlock(QPage, { padding: "" }, {
+        default: withCtx(() => [
+          createVNode(QCard, { class: "q-mb-sm" }, {
+            default: withCtx(() => [
+              createVNode(QCardSection, { class: "text-center" }, {
+                default: withCtx(() => [
+                  createVNode(QIcon, {
+                    name: "task",
+                    size: "xl",
+                    class: "text-green"
+                  }),
+                  createBaseVNode("div", _hoisted_1, toDisplayString(_ctx.$t("questionnaireDonePageTitle")), 1)
+                ]),
+                _: 1
+              }),
+              createVNode(QCardSection, null, {
+                default: withCtx(() => [
+                  createBaseVNode("div", _hoisted_2, toDisplayString(_ctx.$t("questionnaireDonePageText")), 1)
+                ]),
+                _: 1
+              }),
+              createVNode(QCardActions, { align: "center" }, {
+                default: withCtx(() => [
+                  createVNode(QBtn, {
+                    color: "primary",
+                    to: "/infoPage",
+                    onClick: _cache[0] || (_cache[0] = ($event) => unref(fhir).logout())
+                  }, {
+                    default: withCtx(() => [
+                      createTextVNode("Logout")
+                    ]),
+                    _: 1
+                  })
+                ]),
+                _: 1
+              })
+            ]),
+            _: 1
+          })
+        ]),
+        _: 1
+      });
+    };
+  }
+};
+export { _sfc_main as default };
